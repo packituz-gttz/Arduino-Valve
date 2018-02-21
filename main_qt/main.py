fa85e8a9aea34e5b478a3d3a434a426890219ba5
packituz <packituz94gttz@gmail.com>
2018-02-21 17:23:06 -0600

    GUI modifications and backups.

--- main_qt/main.py
+++ main_qt/main.py
@@ -25,14 +25,14 @@ def main():
 
     app.processEvents()
     # LOAD, QApplication
-#    import mainWindow
+    import mainWindow
     import Valves_Main
     import time
     app.processEvents()
     app.setOrganizationName('Gatituz PK')
     app.setOrganizationDomain('http://gatituzmes-server.duckdns.org/')
     app.processEvents()
-    # window = mainWindow.MainWindowStart()
+#    window = mainWindow.MainWindowStart()
     window = Valves_Main.MainWindowStart()
     app.setWindowIcon(QIcon(":/logo.png"))
     app.processEvents()
